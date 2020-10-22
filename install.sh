3c2409a0805a3a6842e6692fce46812c0f48983b
Sam Thursfield <sam@afuera.me.uk>
2020-10-22 16:54:34 +0200

    Remove `read` from install.sh
    This causes problems when called from other scripts.

--- install.sh
+++ install.sh
@@ -79,5 +79,3 @@ rm "${PLUGIN_PATH}${SCRIPT_NAME}"
 if [ ! -f $GLIB_DIR$GLIB_SCHEME ]; then
     echo "Installing the glib schema (password needed)" && sudo cp "${PLUGIN_PATH}${GLIB_SCHEME}" "$GLIB_DIR" && sudo glib-compile-schemas "$GLIB_DIR"
 fi
-
-read -p "Script execution ended, press [Enter] key to exit..."
